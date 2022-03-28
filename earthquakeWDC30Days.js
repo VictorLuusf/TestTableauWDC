439316a5260e61cf15af6a74d5af3beab7ec2bcd
VictorL <42594650+VictorLuusf@users.noreply.github.com>
2022-03-28 11:56:11 -0700

    commit

---
+++ earthquakeWDC30Days.js
@@ -0,0 +1,65 @@
+(function() {
+    // Create the connector object
+    var myConnector = tableau.makeConnector();
+
+    // Define the schema
+    myConnector.getSchema = function(schemaCallback) {
+        var cols = [
+          {id: "mag", alias: "magnitude",
+          dataType: tableau.dataTypeEnum.float},
+
+          {id: "title", alias: "title",
+            dataType: tableau.dataTypeEnum.string},
+
+          {id: "url", alias: "url",
+            dataType: tableau.dataTypeEnum.string},
+
+          {id: "lat", alias: "latitude",
+            dataType: tableau.dataTypeEnum.float},
+
+          {id: "lon", alias: "longitude",
+            dataType: tableau.dataTypeEnum.float}
+      ];
+
+        var tableInfo = {
+            id: "earthquakeFeed",
+            alias: "All Earthquakes in the past 30 days",
+            columns: cols
+        };
+
+        schemaCallback([tableInfo]);
+    };
+
+    // Download the data
+    myConnector.getData = function(table, doneCallback) {
+
+        $.getJSON("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson", function(resp) {
+            var feat = resp.features,
+                tableData = [];
+
+            // Iterate over the JSON object
+            for (var i = 0, len = feat.length; i < len; i++) {
+                mag = feat[i].properties.mag;
+                title = feat[i].properties.title;
+                url = feat[i].properties.url;
+                lon = feat[i].geometry.coordinates[0];
+                lat = feat[i].geometry.coordinates[1];
+
+                });
+            }
+
+            table.appendRows(tableData);
+            doneCallback();
+        });
+    };
+
+    tableau.registerConnector(myConnector);
+
+    // Create event listeners for when the user submits the form
+    $(document).ready(function() {
+        $("#submitButton").click(function() {
+            tableau.connectionName = "USGS Earthquake Feed"; // This will be the data source name in Tableau
+            tableau.submit(); // This sends the connector object to Tableau
+        });
+    });
+})();
